efd26d22ebf575bfbd3e5d941b7df68fe45b3ca4
Clopy <clopy@unweb.me>
2012-08-27 09:05:37 -0700

    added layer

---
+++ src/criticalcommons/skin/browser/interfaces.py
@@ -0,0 +1,5 @@
+from plonetheme.classic.browser.interfaces import IThemeSpecific as IClassicTheme
+
+
+class IThemeSpecific(IClassicTheme):
+    """theme-specific layer"""
--- src/criticalcommons/skin/interfaces/__init__.py
+++
@@ -1,2 +0,0 @@
-from criticalcommonsskinlayer import ICriticalcommonsSkinLayer
-
