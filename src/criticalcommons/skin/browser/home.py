811994bf1d990e736e70b3ad3a1acdc2fb7650d3
Clopy <clopy@unweb.me>
2012-08-27 10:02:41 -0700

    Override homepage, remove left portlet manager

--- src/criticalcommons/skin/browser/home.py
+++ src/criticalcommons/skin/browser/home.py
@@ -1,5 +1,27 @@
 from Products.Five import BrowserView
 
+from zope.component import queryMultiAdapter
+from Products.CMFCore.utils import getToolByName
+
+from random import shuffle
+
 class HomePage(BrowserView):
     u"""This browser view is used to replace the home page
     """
+
+    limit_featured = 5
+
+    @property
+    def featured_videos(self):
+        catalog = getToolByName(self.context, 'portal_catalog')
+        filtering = dict(portal_type='PlumiVideo',
+                 sort_on='effective',
+                 sort_order='reverse',
+                 review_state='featured',
+                         limit=self.limit_featured)
+        brains = catalog(filtering)[:self.limit_featured]
+        shuffled_videos = list(brains)
+        shuffle(shuffled_videos)
+        while len(shuffled_videos) < self.limit_featured:
+            shuffled_videos.append(None)
+        return shuffled_videos
