baf35f6b9d88334fcbd5306b4b1857fe2345f2cf
Mike Muzurakis <clopy@unweb.me>
2012-08-30 16:20:58 +0300

    style and positioning of the add commentary form

--- src/criticalcommons/skin/browser/interfaces.py
+++ src/criticalcommons/skin/browser/interfaces.py
@@ -1,5 +1,8 @@
 from plonetheme.classic.browser.interfaces import IThemeSpecific as IClassicTheme
-
+from zope.viewlet.interfaces import IViewletManager
 
 class IThemeSpecific(IClassicTheme):
     """theme-specific layer"""
+
+class IAddCommentary(IViewletManager):
+    """ A viewlet manager to display the commentary form  """
