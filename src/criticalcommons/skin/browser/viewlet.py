3b605d9b44f877beba32ab1cec62eb4d8fa26780
Dimitris Moraitis <dimo@unweb.me>
2012-08-30 05:26:52 -0700

    add commentary viewlet

---
+++ src/criticalcommons/skin/browser/viewlet.py
@@ -0,0 +1,19 @@
+from Acquisition import aq_inner
+from zope.interface import alsoProvides
+from plone.z3cform.interfaces import IWrappedForm
+
+from z3c.form.interfaces import IFormLayer
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.layout.viewlets import ViewletBase
+from plone.z3cform import z2
+from criticalcommons.content.forms import CommentaryForm
+
+class CommentaryViewlet(ViewletBase):
+    index = ViewPageTemplateFile('templates/commentaryviewlet.pt')
+    label = 'Add Commentary'
+    def update(self):
+        super(CommentaryViewlet, self).update()
+        z2.switch_on(self, request_layer=IFormLayer)
+        self.form = CommentaryForm(aq_inner(self.context), self.request)
+        alsoProvides(self.form, IWrappedForm)
+        self.form.update()
