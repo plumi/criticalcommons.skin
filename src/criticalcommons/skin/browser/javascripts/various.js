4d0af02f961d26288fee53283d643ea56e01402b
Clopy <clopy@unweb.me>
2012-09-16 09:58:35 -0700

    prevent enter key from submitting forms

--- src/criticalcommons/skin/browser/javascripts/various.js
+++ src/criticalcommons/skin/browser/javascripts/various.js
@@ -12,3 +12,12 @@ $("#form\\.usertype").live('change', function() {
         $("#formfield-form-home_page").show();
    }
 });
+
+$(document).ready(function() {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+});
