71e59f6394a334e033daaa7d27d0530039c4ee89
Clopy <clopy@unweb.me>
2012-09-18 02:29:21 -0700

    override registration apply handler

---
+++ src/criticalcommons/skin/browser/forms.py
@@ -0,0 +1,42 @@
+from zope.formlib import form
+from plone.app.users.browser.register import RegistrationForm
+from criticalcommons.skin.browser.interfaces import IMyRegistrationForm
+from zope.interface import implements
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.app.component.hooks import getSite
+from zope.component import getUtility
+from Products.CMFCore.utils import getToolByName
+from criticalcommons.content import _
+
+class MyRegistrationForm(RegistrationForm):
+    """ Subclass the standard registration form
+    """
+
+    implements(IMyRegistrationForm)
+
+    template = ViewPageTemplateFile('templates/register_form.pt')
+
+    @property
+    def form_fields(self):
+        myfields = super(MyRegistrationForm, self).form_fields
+        return myfields
+
+
+    @form.action(_(u'label_register', default=u'Register'),
+                 validator='validate_registration', name=u'register')
+    def action_join(self, action, data):
+        self.handle_join_success(data)
+        if data['usertype'] == 'Advanced user':
+            portal = getSite()
+            mail_host = getToolByName(portal, 'MailHost')
+            sender = data['email']
+            mto = portal.getProperty('email_from_address')
+
+            subject = 'User: ' + data['username'] + ' requested advanced status'
+            message = 'Hi. The user ' + data['username'] + 'has requested to become an advanced user' + '\n\n'
+            message += '\n\n' + 'Visit the user overview to grant the role:' + portal.absolute_url() + '/@@usergroup-userprefs' + '\n\n'
+            mail_host.send(message, mto, sender, subject, charset='utf-8')
+        
+        # XXX Return somewhere else, depending on what
+        # handle_join_success returns?
+        return self.context.unrestrictedTraverse('registered')()
--- src/criticalcommons/skin/browser/interfaces.py
+++ src/criticalcommons/skin/browser/interfaces.py
@@ -1,8 +1,13 @@
 from plonetheme.classic.browser.interfaces import IThemeSpecific as IClassicTheme
 from zope.viewlet.interfaces import IViewletManager
+from zope.interface import Interface
 
 class IThemeSpecific(IClassicTheme):
     """theme-specific layer"""
 
 class IAddCommentary(IViewletManager):
     """ A viewlet manager to display the commentary form  """
+
+class IMyRegistrationForm(Interface):
+    """Marker interface for my custom registration form
+    """
