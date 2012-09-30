cf7d1c8656c16c49a43446bc2171955731570c63
Mike Muzurakis <clopy@unweb.me>
2012-09-30 14:51:02 +0300

    don't prevent enter in forms

--- src/criticalcommons/skin/browser/javascripts/various.js
+++ src/criticalcommons/skin/browser/javascripts/various.js
@@ -15,7 +15,7 @@ $("#form\\.usertype").live('change', function() {
 
 $(document).ready(function() {
   $(window).keydown(function(event){
-    if(event.keyCode == 13) {
+    if(event.keyCode == 13 && $(document.activeElement).attr('id') = "form-widgets-Title") {
       event.preventDefault();
       return false;
     }
